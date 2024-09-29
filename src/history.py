bb121c86d7d0a31a71426ed91a258e59cb3a7e20
Kamil Lewandowski <kamlew555@gmail.com>
2024-09-29 20:04:57 +0200

    FEAT: add Record class with OperationType enum and datetime for operation tracking

---
+++ src/history.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from enum import Enum
+from src.files import JSONManager
+
+
+class OperationType(Enum):
+    SAVING = "Saving"
+    ENCRYPTING = "Encrypting"
+    DECRYPTING = "Decrypting"
+    LOADING = "Loading"
+
+
+@dataclass
+class Record:
+    operation: OperationType
+    input_text: str
+    output_text: str
+    shift: int
+    time: datetime = datetime.now()
+
+    def __str__(self):
+        message = (
+            f"Time: {self.time.strftime("%m/%d/%Y, %H:%M:%S")} \n"
+            f"Operation: {self.operation.value} \n"
+            f"Original text: {self.input_text}, Result: {self.output_text} shift: {self.shift}\n"
+            f"Json: {JSONManager.load_from_json('history.json')}\n"
+        )
+        return message
+
+    def to_dict(self):
+        data = asdict(self)
+        data['operation'] = self.operation.value
+        data['time'] = self.time.strftime('%Y-%m-%d %H:%M:%S')
+        return data
